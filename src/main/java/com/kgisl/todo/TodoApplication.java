74f84ece69fc3aff7e66e56d2daab5b4ef83d985
kishore <33714005+kishorevk02@users.noreply.github.com>
2018-02-16 15:18:06 +0530

    Update TodoApplication.java

--- src/main/java/com/kgisl/todo/TodoApplication.java
+++ src/main/java/com/kgisl/todo/TodoApplication.java
@@ -1,18 +1,18 @@
-// package com.kgisl.todo;
+package com.kgisl.todo;
 
-// import org.h2.server.web.WebServlet;
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 
-// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 
-// @SpringBootApplication
-// public class TodoApplication {
+@SpringBootApplication
+public class TodoApplication {
 
-// 	public static void main(String[] args) {
+	public static void main(String[] args) {
 // 		SpringApplication.run(TodoApplication.class, args);
-// 	}
+	}
 // 	@Bean
 // 	public ServletRegistrationBean h2servletRegistration() {
 // 		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
